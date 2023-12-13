7fd839a017dbeb0b09fd53faefdfc1227116902a
sourin <sourin00@gmail.com>
2023-12-13 08:34:41 +0530

    Initial commit: Test case with Compile Time Error

---
+++ CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testCalculator() {
+        assertEquals(1, CalculatorTest.sum(""));
+    }
+
+}
