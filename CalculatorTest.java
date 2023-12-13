a02bb471f61650a463419198576cad86d651e4fa
sourin <sourin00@gmail.com>
2023-12-13 18:42:15 +0530

    ADDED: Test case with multi character delimiter failing with PatternSyntaxException

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -18,6 +18,7 @@ public class CalculatorTest {
         Exception e2 = assertThrows(Exception.class, () -> CalculatorTest.sum("-1,-2,-3"));
         assertTrue(e2.getMessage().contains("negatives not allowed -1,-2,-3"));
         assertEquals(2, CalculatorTest.sum("1001,2,10009"));
+        assertEquals(6, CalculatorTest.sum("//****\n1****3****2"));
     }
 
     private static int sum(String s) throws Exception {
