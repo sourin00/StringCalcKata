cd1feaaeeac9a6c1f3590eca3a236099754698ad
sourin <sourin00@gmail.com>
2023-12-13 08:41:24 +0530

    ADDED: Test case with single number failing

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -7,6 +7,7 @@ public class CalculatorTest {
     @Test
     public void testCalculator() {
         assertEquals(0, CalculatorTest.sum(""));
+        assertEquals(1, CalculatorTest.sum("1"));
     }
 
     private static int sum(String s) {
